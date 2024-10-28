5f834ea3c22807c26cf7338ee5dec60f2363fc76
Min Thuta Naing <6531503157@lamduan.mfu.ac.th>
2024-10-28 10:57:16 +0700

    nainai

--- smart-hospital/vue.config.js
+++ smart-hospital/vue.config.js
@@ -1,9 +1,24 @@
-const { defineConfig } = require('@vue/cli-service')
-  module.exports = {
-    // Options that Vue CLI expects
-    publicPath: '/',
-    devServer: {
-      proxy: 'http://localhost:8080',
+// const { defineConfig } = require('@vue/cli-service')
+//   module.exports = {
+//     // Options that Vue CLI expects
+//     publicPath: '/',
+//     devServer: {
+//       proxy: 'http://localhost:8080',
+//     },
+//     lintOnSave: false, // Example of turning off linting on save
+//   };
+const { defineConfig } = require('@vue/cli-service');
+module.exports = defineConfig({
+  publicPath: '/',
+  devServer: {
+    proxy: {
+      '/api': {  // Only proxy certain paths if possible, rather than all
+        target: 'http://localhost:8080',
+        ws: true,
+        changeOrigin: true,
+      },
     },
-    lintOnSave: false, // Example of turning off linting on save
-  };
+    hot: true,  // Re-enable hot reload
+  },
+  lintOnSave: false,
+});
