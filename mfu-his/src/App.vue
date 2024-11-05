ffb76bf9b9d9be34092c84ef8221d9d6400b5a62
Min Thuta Naing <6531503157@lamduan.mfu.ac.th>
2024-11-05 10:45:51 +0700

    nainai

--- mfu-his/src/App.vue
+++ mfu-his/src/App.vue
@@ -1,17 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <header>
+      <div class="logo-container">
+        <!-- Display the hospital logo -->
+        <img src="@/assets/hospital_logo.png" alt="Hospital Logo" class="hospital-logo" />
+      </div>
+      <div class="title-container">
+        <!-- Display the hospital information system text -->
+        <h1>MFU Hospital Information System</h1>
+      </div>
+    </header>
+    <WelcomePage />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WelcomePage from './components/WelcomePage.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    WelcomePage,
+  },
+};
 </script>
 
 <style>
@@ -21,6 +32,34 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
+}
+
+/* Header styles */
+header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  background-color: #4DAAB3; /* Updated color */
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+/* Logo styling */
+.logo-container {
+  margin-right: 20px;
+}
+
+.hospital-logo {
+  height: 60px;
+  width: auto;
+}
+
+/* Title styling */
+.title-container h1 {
+  font-size: 24px;
+  color: white;
+  margin: 0;
+  font-weight: 600;
 }
 </style>
---
+++ mfu-his/src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import WelcomePage from '@/components/WelcomePage.vue';
+
+
+const routes = [
+    {path: '/', name: 'WelcomePage', component: WelcomePage}, 
+
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+
+export default router;
