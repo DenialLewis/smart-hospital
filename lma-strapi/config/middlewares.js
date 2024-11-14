3de2fccb738b8f52f4a278dcf71fc58dd6289290
Lwin Moe Aung <6531503151@lamduan.mfu.ac.th>
2024-11-14 13:42:50 +0700

    done

--- lma-strapi/config/middlewares.js
+++ lma-strapi/config/middlewares.js
@@ -1,56 +1,12 @@
 module.exports = [
-  {
-    name: 'strapi::logger',
-    resolve: 'strapi::logger',
-    config: {}
-  },
-  {
-    name: 'strapi::errors',
-    resolve: 'strapi::errors',
-    config: {}
-  },
-  {
-    name: 'strapi::security',
-    resolve: 'strapi::security',
-    config: {}
-  },
-  {
-    name: 'strapi::cors',
-    resolve: 'strapi::cors',
-    config: {
-      enabled: true,
-      origin: ['http://localhost:8080'], // Allow access from your Vue.js app
-      headers: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
-    }
-  },
-  {
-    name: 'strapi::poweredBy',
-    resolve: 'strapi::poweredBy',
-    config: {}
-  },
-  {
-    name: 'strapi::query',
-    resolve: 'strapi::query',
-    config: {}
-  },
-  {
-    name: 'strapi::body',
-    resolve: 'strapi::body',
-    config: {}
-  },
-  {
-    name: 'strapi::session',
-    resolve: 'strapi::session',
-    config: {}
-  },
-  {
-    name: 'strapi::favicon',
-    resolve: 'strapi::favicon',
-    config: {}
-  },
-  {
-    name: 'strapi::public',
-    resolve: 'strapi::public',
-    config: {}
-  }
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
